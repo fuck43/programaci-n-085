67e35f4f59053c84f8f7e179ea91ac4728f2825e
fuck43 <ozimandias130@gmail.com>
2025-01-26 14:05:44 -0600

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,40 @@
+# 1. ¿Cuál es la función de la memoria secundaria en una computadora?
+r1 = "Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido."
+print("respuesta:", r1)
+
+# 2. ¿Qué es un programa?
+r2 = "Un conjunto de instrucciones que indican cómo realizar algún tipo de cálculo."
+print("respuesta:", r2)
+
+# 3. ¿Cuál es la diferencia entre un compilador y un intérprete?
+r3 = """La diferencia es que el intérprete traduce y ejecuta el código fuente línea por línea de forma interactiva, 
+mientras que el compilador traduce todo el código fuente a código máquina en un archivo ejecutable para usarlo posteriormente."""
+print("respuesta:", r3)
+
+# 4. ¿Cuál de los siguientes contiene código máquina?
+r4 = "Un programa ejecutable."
+print("respuesta:", r4)
+
+# 5. ¿Qué está mal en el siguiente código?
+r5 = "'primt' está mal escrito. Debería ser 'print'."
+print("respuesta:", r5)
+
+# 6. ¿En qué lugar del computador queda almacenada una variable, como en este caso 'x', después de ejecutar la siguiente línea de Python?: x = 123
+r6 = "Memoria principal."
+print("respuesta:", r6)
+
+# 7. ¿Qué mostrará en pantalla el siguiente programa?
+r7 = "44"
+print("respuesta:", r7)
+
+# 8. Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+r8 = """1. Unidad central de procesamiento: El razonamiento.
+2. Memoria principal: La memoria a corto plazo.
+3. Memoria secundaria: La memoria a largo plazo.
+4. Dispositivos de entrada: Los sentidos (vista, oído, etc.).
+5. Dispositivos de salida: La capacidad del habla."""
+print("respuesta:", r8)
+
+# 9. ¿Cómo puedes corregir un 'Error de sintaxis'?
+r9 = "Los errores de sintaxis se presentan por fallos en la gramática de Python. La solución es revisar la línea indicada y corregir el fallo."
+print("respuesta:", r9)
