62f9131a42df46921184521b83f0c066e45e83da
fuck43 <ozimandias130@gmail.com>
2025-01-26 14:05:23 -0600

    Add files via upload

---
+++ actvida 1.py
@@ -0,0 +1,13 @@
+#Actividad 1
+
+print("hola mundo")
+
+#Actvidad 2
+int()
+float()
+str()
+w=int(input("ingresa el primer numero:"))
+print("Adicion:",a+b)
+print("sustracion",a-b)
+print("multiplicacion",a*b)
+print("division",a/b)
